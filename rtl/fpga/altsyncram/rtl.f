5a7f278048e82eee2e9299c8e9b199caec4f21c4
mballance-sf <matt.ballance@gmail.com>
2017-06-09 09:51:08 -0700

    ""

--- rtl/fpga/altsyncram/rtl.f
+++ rtl/fpga/altsyncram/rtl.f
@@ -2,3 +2,5 @@
 ${MEMORY_PRIMITIVES}/rtl/fpga/altsyncram/generic_rom.v
 ${MEMORY_PRIMITIVES}/rtl/fpga/altsyncram/generic_sram_byte_en.v
 ${MEMORY_PRIMITIVES}/rtl/fpga/altsyncram/generic_sram_line_en.v
+${MEMORY_PRIMITIVES}/rtl/fpga/altsyncram/generic_sram_line_en_dualport.v
+
